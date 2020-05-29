eaa906def3855dae1ae7585130fb74314c94b2b0
John Riley <jriley@williamhill.us>
2020-05-28 23:16:12 -0400

    Added Icons for Status and Issue Type

--- src/panel/JiraSection/JiraSection.js
+++ src/panel/JiraSection/JiraSection.js
@@ -11,16 +11,23 @@ const JiraSection = ({ jiraEntry }) => {
 
   const summary = get(jiraEntry, 'summary');
   const issueTypeName = get(jiraEntry, 'issuetype.name');
+  const statusName = get(jiraEntry, 'status.name');
   return (
     <div id={`jira-section-${ticketKey}`} className="standard-margin full-width">
       <>
         <div className="bottom-border">
           <span className="header-jira-section bold">{ticketKey} - {summary}</span>
           <img
-            className="issue-type-icon"
+            className="aligned-icon left-spaced"
             src={get(jiraEntry, 'issuetype.iconUrl')}
             alt={issueTypeName}
             title={issueTypeName}
+          />
+          <img
+            className="aligned-icon left-spaced"
+            src={get(jiraEntry, 'status.iconUrl')}
+            alt={statusName}
+            title={statusName}
           />
         </div>
         <div className="standard-margin full-width">
--- src/utils/issue-utils.js
+++ src/utils/issue-utils.js
@@ -1,17 +1,10 @@
 import pick from 'lodash/pick';
 
 const jiraTicketFields = [
-  'comment',
-  'created',
   'description',
-  'issuelinks',
   'issuetype',
-  'project',
-  'resolution',
   'status',
-  'subtasks',
   'summary',
-  'updated',
 ];
 
 export const extractIssueDataFromFields = (jiraData = {}) => pick(jiraData, jiraTicketFields);
